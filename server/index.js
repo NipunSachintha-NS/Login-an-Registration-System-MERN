76db0eea0de93d32ec8c7dce06ecfce31199041a
Nipun Sachintha <lvnsachintha@std.appsc.sab.ac.lk>
2024-12-15 08:55:07 +0530

    Password hash

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express =  require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const EmployeeModel = require('./models/Employee')
-
+const bcrypt = require('bcrypt')
 
 const app = express()
 app.use(cors())
@@ -11,22 +11,29 @@ app.use(express.json())
 mongoose.connect('mongodb://localhost:27017/employee');
 
 app.post('/register', (req, res) => {
-    EmployeeModel.create(req.body)
-        .then(employees => res.json(employees))
-        .catch(err => res.json(err))    
-    })
-
+    const {name, email, password} = req.body;
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        EmployeeModel.create({name, email, password:hash})
+            .then(employees => res.json(employees))
+            .catch(err => res.json(err))  
+    }).catch(err => console.log(err.message))
+      
+ })
 
 app.post("/login", (req,res) =>{
     const {email, password} = req.body;
     EmployeeModel.findOne({email:email})
     .then(user => {
         if(user) {
-            if(user.password === password){
-                res.json("Login Succussfuly")
-            } else {
-                res.json("The password is incorrect")
-            }
+            bcrypt.compare(password,user.password, (err, response) =>{   
+                 if(response) {
+                    res.json("Login Succussfuly")     
+                } else {
+                    res.json("The password is incorrect")  
+                }
+          })
+
         } else {
             res.json("No record existed")  
         }
